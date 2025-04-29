44b56a960e5c532974066be7d5fd159ab1c53675
alexf0xdev <glebf78@gmail.com>
2025-04-29 14:48:45 +0500

    fix ListeningNow on small screen

--- src/components/ListeningNow.tsx
+++ src/components/ListeningNow.tsx
@@ -20,7 +20,7 @@ const ListeningNow = () => {
     <div className={cn('relative bg-zinc-800 p-5 mt-10')}>
       <h2 className={cn('text-2xl font-semibold')}>I&apos;m listening now:</h2>
       <div className={cn('flex items-center gap-5 mt-2')}>
-        <div className='relative h-15 w-15'>
+        <div className='relative h-12 sm:h-15 w-12 sm:w-15'>
           <Image
             layout='fill'
             objectFit='cover'
@@ -31,14 +31,14 @@ const ListeningNow = () => {
         <div>
           <h3
             className={cn(
-              'font-semibold max-w-1/5 sm:max-w-lg whitespace-nowrap overflow-hidden text-ellipsis',
+              'text-base sm:text-lg font-semibold max-w-30 sm:max-w-92 whitespace-nowrap overflow-hidden text-ellipsis',
             )}
           >
             {currentlyPlaying.name}
           </h3>
           <p
             className={cn(
-              'text-base max-w-1/5 sm:max-w-lg whitespace-nowrap overflow-hidden text-ellipsis',
+              'text-sm sm:text-base max-w-30 sm:max-w-92 whitespace-nowrap overflow-hidden text-ellipsis',
             )}
           >
             {currentlyPlaying.artists}
